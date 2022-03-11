a7ac02660f913ca22d863ae0b092f3b888022a77
Lim <https://github.com/Ominium/>
2022-03-11 16:08:23 +0900

    소스 수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
-    public static void main(String argc[]){
+    public static void main(String[] args){
         System.out.println("Hello First Java Cording!!");
+
+        System.out.println("Edit Java Cord");
     }
 }
