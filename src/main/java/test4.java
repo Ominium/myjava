3a22ae71f383455e94bb01f361a6783d89108d5e
Lim <gustjr790@gmail.com>
2022-04-12 16:46:36 +0900

    22_04_12 java 다이아몬드 별

--- src/main/java/test4.java
+++ src/main/java/test4.java
@@ -1,22 +1,42 @@
 import java.util.Scanner;
+
 public class test4 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int a = 0;
+        int count = 0;
         Scanner scanner;
         scanner = new Scanner(System.in);
         a = scanner.nextInt();
+        for (int i = 1; i <= a; i=i+2) {
 
-        for(int i=0; i<=a; i++){
-            for(int j= i; a-j >0; j++){
+            for(int j = a/2; j-count>0; j--){
                 System.out.printf(" ");
             }
-            for(int j= 0; j<i; j++){
+            for(int j = 0; j<i; j++){
                 System.out.printf("*");
-
             }
-            System.out.println("");
+            for(int j = a/2; j-count>0; j--){
+                System.out.printf(" ");
+            }
+            count++;
+            System.out.printf("\n");
         }
+        count = a/2 - 1;
 
+        for (int i = a -2; i >= 1; i=i-2) {
+
+            for(int j = a/2; j-count>0; j--){
+                System.out.printf(" ");
+            }
+            for(int j = 0; j<i; j++){
+                System.out.printf("*");
+            }
+            for(int j = a/2; j- count>0; j--){
+                System.out.printf(" ");
+            }
+            count--;
+            System.out.printf("\n");
+        }
 
     }
 }
